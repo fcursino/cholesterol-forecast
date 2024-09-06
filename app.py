e91814f72f4f658e8a73da4887a284b16ad6cde9
fcursino <fabriciocursino09@gmail.com>
2024-09-06 07:03:09 -0300

    creating files to upload project in Render

---
+++ app.py
@@ -0,0 +1,34 @@
+import gradio as gr
+import joblib
+import pandas as pd
+
+modelo = joblib.load('./modelo_colesterol.pkl')
+
+def predict(grupo_sanguineo, fumante, nivel_atividade_fisica, idade, peso, altura):
+  _fumante = 'Sim' if fumante else 'Não'
+  predicao_individual = {
+    'grupo_sanguineo': grupo_sanguineo,
+    'fumante': _fumante,
+    'nivel_atividade_fisica': nivel_atividade_fisica,
+    'idade': idade,
+    'peso': peso,
+    'altura': altura
+  }
+  predict_df = pd.DataFrame(predicao_individual, index=[1])
+  colesterol = modelo.predict(predict_df)
+  return colesterol.reshape(-1)
+
+demo = gr.Interface(
+  fn=predict,
+  inputs=[
+    gr.Radio(['O','A','B','AB']),
+    'checkbox',
+    gr.Radio(['Baixo','Moderado','Alto']),
+    gr.Slider(20, 80, step=1),
+    gr.Slider(40, 160, step=0.1),
+    gr.Slider(150, 200, step=1)
+  ],
+  outputs=['number']
+)
+
+demo.launch()
